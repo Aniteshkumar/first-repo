3f949e2ed568dd386561de985ad07d7cfd786078
Anitesh Kumar <aniteshkumar2002@gmail.com>
2023-02-22 06:38:00 -0500

    sum of any number

---
+++ sum.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter first num :";
+    cin>>a;
+    cout<<"enter second  num :";
+    cin>>b;
+
+    cout<<"sum :"<<a+b;
+}
